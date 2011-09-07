31ed2cf3cafe227048be260c3008481cafd49e1c
U-Athlon5000\User <User@Athlon5000.(none)>
2011-09-07 21:04:14 +0200

    File split up.

---
+++ Scripts/Tools/CloneGitRepo.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Copyright (C) 2011 R. Diez - see the orbuild project for licensing information.
+
+set -o errexit
+source "$(dirname $0)/../ShellModules/StandardShellHeader.sh"
+
+
+if [ $# -ne 4 ]; then
+  abort "Invalid number of command-line arguments. Usage: $0 <git url> <base dir> <sentinel filename>"
+fi
+
+GIT_URL="$1"
+DEST_DIR="$2"
+BASE_DIR="$3"
+SENTINEL_FILENAME="$4"
+
+REPO_DIR="$BASE_DIR/$DEST_DIR"
+
+pushd "$BASE_DIR" >/dev/null
+
+if [ -d "$REPO_DIR" ]; then
+  # As far as I know, "git clone" cannot resume an interrupted transfer,
+  # so delete the existing repository and start from scratch.
+  echo "Deleting previous incomplete or outdated repository clone at \"$REPO_DIR\"..."
+  rm -rf "$REPO_DIR"
+fi
+
+echo "Cloning git repository at URL \"$GIT_URL\"..."
+
+git clone --no-checkout "$GIT_URL" "$DEST_DIR"
+
+if ! [ -d "$REPO_DIR" ]; then
+  abort "The git repository \"$GIT_URL\" has not created the expected subdirectory \"$DEST_DIR\" when checking out in directory \"$BASE_DIR\"."
+fi
+
+popd "$BASE_DIR" >/dev/null
+
+printf "This file acts as a flag that git repository:\n  %s\nwas successfully cloned at location:\n  %s\n" \
+       "$GIT_URL" "$REPO_DIR" >"$SENTINEL_FILENAME"
---
+++ Scripts/Tools/FetchGitRepo.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Copyright (C) 2011 R. Diez - see the orbuild project for licensing information.
+
+set -o errexit
+source "$(dirname $0)/../ShellModules/StandardShellHeader.sh"
+
+
+if [ $# -ne 4 ]; then
+  abort "Invalid number of command-line arguments. Usage: $0 <git url> <dest subdir> <base dir> <sentinel filename>"
+fi
+
+GIT_URL="$1"
+DEST_DIR="$2"
+BASE_DIR="$3"
+SENTINEL_FILENAME="$4"
+
+REPO_DIR="$BASE_DIR/$DEST_DIR"
+
+pushd "$REPO_DIR" >/dev/null
+
+echo "Fetching updated content from git repository at URL \"$GIT_URL\"..."
+
+# POSSIBLE OPTIMISATION: For the purposes of a daily build, maybe we just need to
+#                        download the HEAD branches with something like this:
+#                          git fetch origin +refs/heads/master:refs/remotes/origin/master
+
+git fetch --all
+
+popd >/dev/null
+
+printf "This file acts as a flag that git repository:\n  %s\nwas successfully fetched at this location.\n" \
+       "$GIT_URL" >"$SENTINEL_FILENAME"
