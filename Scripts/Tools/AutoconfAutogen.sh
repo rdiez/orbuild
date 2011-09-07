6f33b281620cc0651523c77a3ba373e9f33dcdf2
U-Athlon5000\User <User@Athlon5000.(none)>
2011-09-07 21:03:08 +0200

    First check-in.

---
+++ Scripts/Tools/AutoconfAutogen.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Copyright (C) 2011 R. Diez - see the orbuild project for licensing information.
+
+set -o errexit
+source "$(dirname $0)/../ShellModules/StandardShellHeader.sh"
+
+
+if [ $# -ne 3 ]; then
+  abort "Invalid number of command-line arguments. Usage: $0 <src dir> <autogen cmd> <sentinel filename>"
+fi
+
+SRC_DIR="$1"
+AUTOGEN_CMD="$2"
+SENTINEL_FILENAME="$3"
+
+if [ -z "$AUTOGEN_CMD" ]; then
+  abort "Empty autogen command."
+fi
+
+pushd "$SRC_DIR" >/dev/null
+
+$AUTOGEN_CMD
+
+echo "Done" >"$SENTINEL_FILENAME"
+
+popd >/dev/null
