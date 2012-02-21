60eb9c9e201a7a40521634bbb0ea7c234af95ae9
U-Athlon5000\User <User@Athlon5000.(none)>
2012-02-21 22:25:20 +0100

    $(1)_SENTINEL_FILENAME -> $(1)_SENTINEL , for consistency.

--- Scripts/MakeModules/RunAndReportTemplates.mk
+++ Scripts/MakeModules/RunAndReportTemplates.mk
@@ -8,13 +8,13 @@
 #  $(2) is used to form the log and sentinel filenames.
 #  $(3) is the command to run
 #
-# In order to trigger this rule, add to some target a dependency to $(1)_SENTINEL_FILENAME .
+# In order to trigger this rule, add to some target a dependency to $(1)_SENTINEL .
 
 define run_and_report_template_variables
 
   $(1)_LOG_FILENAME       := $(ORBUILD_PUBLIC_REPORTS_DIR)/$(2).CmdLog.txt
   $(1)_REPORT_FILENAME    := $(ORBUILD_INTERNAL_REPORTS_DIR)/$(2).Cmd.report
-  $(1)_SENTINEL_FILENAME  := $(ORBUILD_COMMAND_SENTINELS_DIR)/$(2).orbuild-unpack-sentinel
+  $(1)_SENTINEL           := $(ORBUILD_COMMAND_SENTINELS_DIR)/$(2).orbuild-command-sentinel
 
 endef
 
@@ -22,12 +22,12 @@ define run_and_report_template
 
   $(eval $(call run_and_report_template_variables,$(1),$(2)))
 
-  $(value $(1)_SENTINEL_FILENAME):
+  $(value $(1)_SENTINEL):
 	  "$(ORBUILD_TOOLS)/RunAndReport.sh" \
                   "$(2)" \
                   "$(value $(1)_LOG_FILENAME)" \
                   "$(value $(1)_REPORT_FILENAME)" \
                   report-always \
           $(3) && \
-      echo "Command was run successfully." >"$(value $(1)_SENTINEL_FILENAME)"
+      echo "Command was run successfully." >"$(value $(1)_SENTINEL)"
 endef
