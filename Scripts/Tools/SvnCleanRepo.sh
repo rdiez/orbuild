da7ad563ddfffd266e9a4c11d9b1d26a4e8dfc06
R. Diez <User@Athlon5000.(none)>
2012-03-26 21:51:37 +0200

    Fixed exit code 1 when no files to delete.

--- Scripts/Tools/SvnCleanRepo.sh
+++ Scripts/Tools/SvnCleanRepo.sh
@@ -14,6 +14,13 @@ SVN_REPO_DIR="$1"
 
 pushd "$SVN_REPO_DIR" >/dev/null
 
-svn status --no-ignore | grep '^[?I]' | awk '{print $2}' | xargs rm -rf
+
+# Grep yields an exit code of 1 if it finds no matches. Turn that eventual exit code to 0, which
+# bash interprets as success.
+
+svn status --no-ignore \
+  | { set +o errexit; grep '^[?I]'; grep_exit_code=$?; if [ $grep_exit_code -eq 1 ]; then exit 0; else exit $grep_exit_code; fi } \
+  | awk '{print $2}' \
+  | xargs --no-run-if-empty  rm -rf
 
 popd >/dev/null
