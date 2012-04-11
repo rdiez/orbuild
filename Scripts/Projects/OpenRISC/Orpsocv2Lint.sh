c52b8eab2e55c0a4a759905c6b053ef9087e8304
R. Diez <User@Athlon5000.(none)>
2012-04-11 17:49:22 +0200

    Added a lint step for ORPSoC v2.

---
+++ Scripts/Projects/OpenRISC/Orpsocv2Lint.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Copyright (C) 2011 R. Diez - see the orbuild project for licensing information.
+
+set -o errexit
+source "$(dirname $0)/../../ShellModules/StandardShellHeader.sh"
+source "$(dirname $0)/../../ShellModules/PrintCommand.sh"
+
+
+if [ $# -ne 3 ]; then
+  abort "Invalid number of command-line arguments, see the source code for details."
+fi
+
+VERILATOR_BIN_DIR="$1"
+ICARUS_VERILOG_BIN_DIR="$2"
+ORPSOCV2_CHECKOUT_DIR="$3"
+
+printf "\n------------ Verilator lint begin ------------\n\n"
+
+CMD="$VERILATOR_BIN_DIR/bin/verilator"
+CMD+=" --lint-only -Wall --error-limit 10000 -Wno-UNUSED -Wno-fatal"
+CMD+=" -I$ORPSOCV2_CHECKOUT_DIR/sim/vlt"
+CMD+=" -I$ORPSOCV2_CHECKOUT_DIR/rtl/verilog/include"
+CMD+=" -I$ORPSOCV2_CHECKOUT_DIR/bench/verilog/include"
+CMD+=" -y $ORPSOCV2_CHECKOUT_DIR/rtl/verilog/or1200"
+CMD+=" $ORPSOCV2_CHECKOUT_DIR/rtl/verilog/or1200/or1200_top.v"
+
+print_command $CMD
+printf "\n"
+$CMD
+
+printf "\n------------ Verilator lint end ------------\n\n"
+
+printf "\n------------ Icarus Verilog lint begin ------------\n\n"
+
+CMD="$ICARUS_VERILOG_BIN_DIR/bin/iverilog"
+CMD+=" -gno-std-include"
+CMD+=" -Wall -Wno-timescale"
+CMD+=" -g2005"
+CMD+=" -o /dev/null"
+CMD+=" -I$ORPSOCV2_CHECKOUT_DIR/sim/vlt"
+CMD+=" -I$ORPSOCV2_CHECKOUT_DIR/rtl/verilog/include"
+CMD+=" -I$ORPSOCV2_CHECKOUT_DIR/bench/verilog/include"
+CMD+=" -y $ORPSOCV2_CHECKOUT_DIR/rtl/verilog/or1200"
+CMD+=" $ORPSOCV2_CHECKOUT_DIR/rtl/verilog/or1200/or1200_top.v"
+
+print_command $CMD
+printf "\n"
+$CMD
+
+printf "\n------------ Icarus Verilog lint end ------------\n\n"
