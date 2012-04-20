bf00b72903d87f5e8724b133f85be82648497a25
R. Diez <User@Athlon5000.(none)>
2012-04-20 17:14:26 +0200

    Improved Verilog linting of ORPSoC V2

--- Scripts/Projects/OpenRISC/Orpsocv2Lint.sh
+++ Scripts/Projects/OpenRISC/Orpsocv2Lint.sh
@@ -7,29 +7,98 @@ source "$(dirname $0)/../../ShellModules/StandardShellHeader.sh"
 source "$(dirname $0)/../../ShellModules/PrintCommand.sh"
 
 
-if [ $# -ne 3 ]; then
+if [ $# -ne 4 ]; then
   abort "Invalid number of command-line arguments, see the source code for details."
 fi
 
 VERILATOR_BIN_DIR="$1"
 ICARUS_VERILOG_BIN_DIR="$2"
 ORPSOCV2_CHECKOUT_DIR="$3"
+LEVEL="$4"
 
-printf "\n------------ Verilator lint begin ------------\n\n"
+LINT_WITH_VERILATOR=true
 
-CMD="$VERILATOR_BIN_DIR/bin/verilator"
-CMD+=" --lint-only -language 1364-2001 -Wall --error-limit 10000 -Wno-UNUSED -Wno-fatal"
-CMD+=" -I$ORPSOCV2_CHECKOUT_DIR/sim/vlt"
-CMD+=" -I$ORPSOCV2_CHECKOUT_DIR/rtl/verilog/include"
-CMD+=" -I$ORPSOCV2_CHECKOUT_DIR/bench/verilog/include"
-CMD+=" -y $ORPSOCV2_CHECKOUT_DIR/rtl/verilog/or1200"
-CMD+=" $ORPSOCV2_CHECKOUT_DIR/rtl/verilog/or1200/or1200_top.v"
+OR1200_INCLUDE=" -I$ORPSOCV2_CHECKOUT_DIR/sim/vlt"
+OR1200_INCLUDE+=" -I$ORPSOCV2_CHECKOUT_DIR/rtl/verilog/include"
+OR1200_INCLUDE+=" -I$ORPSOCV2_CHECKOUT_DIR/bench/verilog/include"
+OR1200_INCLUDE+=" -y $ORPSOCV2_CHECKOUT_DIR/rtl/verilog/or1200"
 
-print_command $CMD
-printf "\n"
-$CMD
+ORPSOC_INCLUDE=" -y $ORPSOCV2_CHECKOUT_DIR/rtl/verilog/clkgen"
+ORPSOC_INCLUDE+=" -y $ORPSOCV2_CHECKOUT_DIR/rtl/verilog/arbiter"
+ORPSOC_INCLUDE+=" -y $ORPSOCV2_CHECKOUT_DIR/rtl/verilog/uart16550"
+ORPSOC_INCLUDE+=" -y $ORPSOCV2_CHECKOUT_DIR/rtl/verilog/intgen"
+ORPSOC_INCLUDE+=" -y $ORPSOCV2_CHECKOUT_DIR/rtl/verilog/ram_wb"
+ORPSOC_INCLUDE+=" -y $ORPSOCV2_CHECKOUT_DIR/rtl/verilog/dbg_if"
+ORPSOC_INCLUDE+=" -y $ORPSOCV2_CHECKOUT_DIR/rtl/verilog/jtag_tap"
 
-printf "\n------------ Verilator lint end ------------\n\n"
+
+TESTBENCH_INCLUDE=" -I$ORPSOCV2_CHECKOUT_DIR/sim/bin"
+TESTBENCH_INCLUDE+=" -y $ORPSOCV2_CHECKOUT_DIR/rtl/verilog/orpsoc_top"
+TESTBENCH_INCLUDE+=" -y $ORPSOCV2_CHECKOUT_DIR/bench/verilog"
+
+
+lint_or1200 ()
+{
+  echo "Linting or1200"
+  ALL_INCLUDES="$OR1200_INCLUDE"
+  TOP_LEVEL_FILE="$ORPSOCV2_CHECKOUT_DIR/rtl/verilog/or1200/or1200_top.v"
+}
+
+
+lint_orpsoc ()
+{
+  echo "Linting orpsoc"
+  TOP_LEVEL_FILE="$ORPSOCV2_CHECKOUT_DIR/rtl/verilog/orpsoc_top/orpsoc_top.v"
+  ALL_INCLUDES="$OR1200_INCLUDE"
+  ALL_INCLUDES+="$ORPSOC_INCLUDE"
+}
+
+
+lint_testbench ()
+{
+  echo "Linting the testbench"
+
+  # The test bench only works with Icarus Verilog.
+  LINT_WITH_VERILATOR=false
+
+  TOP_LEVEL_FILE+="$ORPSOCV2_CHECKOUT_DIR/bench/verilog/orpsoc_testbench.v"
+  ALL_INCLUDES="$OR1200_INCLUDE"
+  ALL_INCLUDES+="$ORPSOC_INCLUDE"
+  ALL_INCLUDES+="$TESTBENCH_INCLUDE"
+}
+
+
+case "$LEVEL" in
+  or1200)    lint_or1200;;
+  orpsoc)    lint_orpsoc;;
+  testbench) lint_testbench;;
+  *)         abort "Invalid lint level argument '$LEVEL'.";;
+esac
+
+
+VERILATOR_LINT_FAILED=0
+
+if $LINT_WITH_VERILATOR; then
+
+  printf "\n------------ Verilator lint begin ------------\n\n"
+
+  CMD="$VERILATOR_BIN_DIR/bin/verilator"
+  CMD+=" --lint-only -language 1364-2001 -Wall --error-limit 10000 -Wno-UNUSED -Wno-fatal"
+  CMD+=" $ALL_INCLUDES "
+  CMD+=" $TOP_LEVEL_FILE "
+
+  print_command $CMD
+  printf "\n"
+
+  if ! $CMD; then
+    VERILATOR_LINT_FAILED=1
+  fi
+
+  printf "\n------------ Verilator lint end ------------\n\n"
+else
+  printf "\n------------ Linting with Verilator skipped ------------\n\n"
+fi
+
 
 printf "\n------------ Icarus Verilog lint begin ------------\n\n"
 
@@ -38,14 +107,34 @@ CMD+=" -gno-std-include"
 CMD+=" -Wall -Wno-timescale"
 CMD+=" -g2001"
 CMD+=" -o /dev/null"
-CMD+=" -I$ORPSOCV2_CHECKOUT_DIR/sim/vlt"
-CMD+=" -I$ORPSOCV2_CHECKOUT_DIR/rtl/verilog/include"
-CMD+=" -I$ORPSOCV2_CHECKOUT_DIR/bench/verilog/include"
-CMD+=" -y $ORPSOCV2_CHECKOUT_DIR/rtl/verilog/or1200"
-CMD+=" $ORPSOCV2_CHECKOUT_DIR/rtl/verilog/or1200/or1200_top.v"
+CMD+=" $ALL_INCLUDES "
+CMD+=" $TOP_LEVEL_FILE "
 
 print_command $CMD
 printf "\n"
-$CMD
+
+if ! $CMD; then
+  ICARUS_VERILOG_LINT_FAILED=1
+else
+  ICARUS_VERILOG_LINT_FAILED=0
+fi
 
 printf "\n------------ Icarus Verilog lint end ------------\n\n"
+
+AT_LEAST_ONE_FAILED=false
+
+if [ $VERILATOR_LINT_FAILED -ne 0 ]; then
+  echo "Error: Verilator lint failed, see above for details."
+  AT_LEAST_ONE_FAILED=true
+fi
+
+if [ $ICARUS_VERILOG_LINT_FAILED -ne 0 ]; then
+  echo "Error: Icarus Verilog lint failed, see above for details."
+  AT_LEAST_ONE_FAILED=true
+fi
+
+if $AT_LEAST_ONE_FAILED; then
+  exit 1
+else
+  exit 0
+fi
