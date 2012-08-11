fb61448d5f2be297fb04db7ac512d07f4606f494
rdiez <rdiez@rdiez-C660.(none)>
2012-08-11 22:26:06 +0200

    Added a comment.

--- Scripts/ShellModules/PrintCommand.sh
+++ Scripts/ShellModules/PrintCommand.sh
@@ -1,6 +1,24 @@
 
 # Print the executed command with proper quoting, so that the user can
 # copy-and-paste the command from the log file and expect it to work.
+#
+# Examples of usage:
+#
+#   REMOVE_SOME_PREFIX_BEFORE_EXECUTING_WITH_REST_OF_ARGS="$1"
+#   shift
+#   print_command "$@"
+#   "$@"
+#
+#   CMD="my-command --my-arg"
+#   print_command $CMD
+#   $CMD
+#
+# However, if you use 'eval' to run commands, do not use 'print_command', use 'echo' instead,
+# or 'printf' in order to print extra newline characters portably:
+#
+#   CMD="my-command --my-arg"
+#   printf "$CMD\n\n"
+#   eval "$CMD"
 
 print_command ()
 {
