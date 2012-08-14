fa01e45d0341461af09e840e36ee6ab4207c7657
rdiez <rdiez@rdiez-C660.(none)>
2012-08-14 19:57:14 +0200

    Remove sourcing from PrintCommand.sh

--- Scripts/Projects/OpenRISC/TestSuite/autogen.sh
+++ Scripts/Projects/OpenRISC/TestSuite/autogen.sh
@@ -4,7 +4,6 @@
 
 set -o errexit
 source "$(dirname $0)/../../../ShellModules/StandardShellHeader.sh"
-source "$(dirname $0)/../../../ShellModules/PrintCommand.sh"
 source "$(dirname $0)/../../../ShellModules/FileUtils.sh"
 
 # set -x  # Trace the commands as they are executed.
