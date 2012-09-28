1b7fdd2c0f9fac1126b889636c16ecef3e68a940
rdiez <rdiez@TOSHIBA.(none)>
2012-09-28 20:47:44 +0200

    Ignore SIGPIPE, as needed by the DPI modules which use sockets. Several other improvements.

--- Scripts/Projects/OpenRISC/OR10/TestBench/test_bench_verilator_driver.cpp
+++ Scripts/Projects/OpenRISC/OR10/TestBench/test_bench_verilator_driver.cpp
@@ -5,8 +5,11 @@
 #define __STDC_FORMAT_MACROS  // For PRIu64
 
 #include <stdint.h>
+#include <signal.h>
 #include <limits.h>
 #include <inttypes.h>  // For PRIu64
+
+#include <stdexcept>
 
 #include "Vtest_bench.h"
 
@@ -16,44 +19,80 @@ static uint64_t current_simulation_time = 0;
 double sc_time_stamp ()
 {
   return double( current_simulation_time );
-} 
+}
+
+
+static void ignore_sigpipe ( void )
+{
+  // Signal SIGPIPE can happen if one of the DPI modules write data to a socket and the remote side has already
+  // closed the connection. Such errors will be handled properly by the writing routines, but we must ignore
+  // the resulting signal, or it will kill the process immediately.
+
+  struct sigaction act;
+
+  act.sa_handler = SIG_IGN;
+  act.sa_flags   = 0;
+
+  if ( 0 != sigemptyset( &act.sa_mask ) )
+    throw std::runtime_error( "Error setting signal mask." );
+
+  if ( 0 != sigaction( SIGPIPE, &act, NULL ) )
+    throw std::runtime_error( "Error setting signal handler." );
+
+  if ( 0 != siginterrupt( SIGPIPE, 0 ) )
+    throw std::runtime_error( "Error setting signal interrupt." );
+}
 
 
 int main ( int argc, char ** argv, char ** env )
 {
-  Verilated::commandArgs( argc, argv );  // Remember args for $value$plusargs() and the like.
-  Verilated::debug( 0 );  // Comment from Verilator example: "We compiled with it on for testing, turn it back off"
+  const uint8_t RESET_ASSERTED   = 1;
+  const uint8_t RESET_DEASSERTED = 0;
 
-  Vtest_bench * const top = new Vtest_bench;
-
-  const uint64_t reset_duration = 0;  // Number of rising clock edges the reset signal will be asserted,
-                                      // set it to 0 in order to start the simulation without asserting the reset signal
-                                      // (handy to simulate FPGA designs without user reset signal).
-  
-  top->reset = reset_duration > 0 ? 1 : 0;
-
-  while ( !Verilated::gotFinish() )
+  try
   {
-    // printf( "Iteration, clock: current_simulation_time %" PRIu64 "\n", current_simulation_time );
-    // printf( "Reset: %d\n", top->reset );
-    
-    if ( current_simulation_time >= reset_duration * 2 )
+    ignore_sigpipe();
+
+    Verilated::commandArgs( argc, argv );  // Remember args for $value$plusargs() and the like.
+    Verilated::debug( 0 );  // Comment from Verilator example: "We compiled with it on for testing, turn it back off"
+
+    Vtest_bench * const top = new Vtest_bench;
+
+    const uint64_t reset_duration = 0;  // Number of rising clock edges the reset signal will be asserted,
+                                        // set it to 0 in order to start the simulation without asserting the reset signal
+                                        // (handy to simulate FPGA designs without user reset signal).
+
+    top->reset = reset_duration > 0 ? RESET_ASSERTED : RESET_DEASSERTED;
+
+    while ( !Verilated::gotFinish() )
     {
-      top->reset = 0;  // Deassert reset.
+      // printf( "Iteration, clock: current_simulation_time %" PRIu64 "\n", current_simulation_time );
+      // printf( "Reset: %d\n", top->reset );
+
+      if ( current_simulation_time >= reset_duration * 2 )
+      {
+        top->reset = RESET_DEASSERTED;  // Deassert reset.
+      }
+
+      top->clock = !top->clock;
+
+      top->eval();
+
+      ++current_simulation_time;
+
+      // Provide an early warning against the remote possibility of a wrap-around.
+      assert( current_simulation_time < UINT64_MAX / 100000 );
     }
 
-    top->clock = !top->clock;
-    
-    top->eval();
-    
-    ++current_simulation_time;
+    top->final();
 
-    // Provide an early warning against the remote possibility of a wrap-around.
-    assert( current_simulation_time < UINT64_MAX / 100000 );
+    delete top;
+
+    return 0;
   }
-
-  top->final();
-
-  return 0;
+  catch ( const std::exception & e )
+  {
+    fprintf( stderr, "%s%s\n", "ERROR: ", e.what() );
+    return 1;
+  }
 }
-
