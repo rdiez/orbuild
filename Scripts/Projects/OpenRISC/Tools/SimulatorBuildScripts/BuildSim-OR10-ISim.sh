a7f7c84392e89183031778644d742ec861a28786
rdiez <rdiez@TOSHIBA.(none)>
2012-10-05 18:55:36 +0200

    Added path to UART module.

--- Scripts/Projects/OpenRISC/Tools/SimulatorBuildScripts/BuildSim-OR10-ISim.sh
+++ Scripts/Projects/OpenRISC/Tools/SimulatorBuildScripts/BuildSim-OR10-ISim.sh
@@ -35,6 +35,9 @@ declare -a INCLUDE_PATHS=(
     --include      $OR10_BASE_DIR/CPU
     --sourcelibdir $OR10_BASE_DIR/CPU
 
+    --include      $OR10_BASE_DIR/UART
+    --sourcelibdir $OR10_BASE_DIR/UART
+
     --include      $OR10_BASE_DIR/Misc
     --sourcelibdir $OR10_BASE_DIR/Misc
   )
