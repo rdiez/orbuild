379567e8a20f79044a042256d9558e7aa8f8269a
rdiez <rdiez@TOSHIBA.(none)>
2012-11-16 19:04:01 +0100

    Added rtrim() and ltrim().

--- Scripts/Projects/OpenRISC/OR10/GdbToJtagBridge/src/string_utils.h
+++ Scripts/Projects/OpenRISC/OR10/GdbToJtagBridge/src/string_utils.h
@@ -3,6 +3,7 @@
 #define STRING_UTILS_H_INCLUDED
 
 #include <string>
+#include <algorithm>
 
 std::string format_msg ( const char * format_str, ... );
 std::string format_errno_msg ( int errno_val,
@@ -13,5 +14,20 @@ std::string ip_address_to_text ( const struct in_addr * addr );
 
 bool str_starts_with ( const std::string * str, const std::string * prefix );
 bool str_remove_prefix ( std::string * str, const std::string *  prefix );
+
+inline void rtrim ( std::string * const s )
+{
+  s->erase( std::find_if( s->rbegin(),
+                          s->rend(),
+                          std::not1(std::ptr_fun<int, int>(std::isspace))).base(),
+            s->end() );
+}
+
+inline void ltrim ( std::string * const s )
+{
+  s->erase( s->begin(),
+            std::find_if( s->begin(), s->end(),
+                          std::not1( std::ptr_fun<int, int>(std::isspace) ) ) );
+}
 
 #endif  // Include this header only once.
